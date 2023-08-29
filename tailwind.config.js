7f7c75d72ab25419597d77bd2fcc25d202f0c83a
andrewboldrin <andrewboldrin@hotmail.com>
2023-08-29 20:03:36 -0300

    chore: setting custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,24 @@ export default {
           2: '28px',
         },
         subheading: '18px',
+      },
+      colors: {
+        orange: {
+          primary: '#FF4017',
+          secondary: '#FF623C',
+        },
+        red: {
+          primary: '#C51E00',
+          secondary: '#E52500',
+        },
+        dark: {
+          primary: '#27282F',
+          secondary: '#303139',
+          contrast: {
+            dark: '#3E3F48',
+            light: '#51535C',
+          }
+        }
       }
     },
   },
