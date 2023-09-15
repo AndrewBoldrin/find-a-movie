30e4bcddf33b4861e347d5efc701dfbd938ac55c
AndrewBoldrin <andrewboldrin@hotmail.com>
2023-09-15 00:45:17 -0300

    chore: add api config and endpoints

---
+++ src/api/config.ts
@@ -0,0 +1,7 @@
+export const API = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  token: import.meta.env.VITE_API_TOKEN,
+  baseURL: 'https://api.themoviedb.org/3',
+  baseImageURL: 'https://image.tmdb.org/t/p/w',
+  language: 'pt-BR',
+}
---
+++ src/api/endpoints.ts
@@ -0,0 +1,19 @@
+import { API } from './config'
+
+export function setQueryParams(page: number) {
+    return `?language=${API.language}&page=${page}`
+}
+
+export const endpoints = Object.freeze({
+    section: {
+        popular: '/movie/popular',
+        top_rated: '/movie/top_rated',
+        upcoming: '/movie/upcoming',
+    },
+    genre: {   
+        genres: '/genre/movie/list',
+    },
+    movies: {
+        movie: '/movie/',
+    }
+})
