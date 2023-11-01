603601ac65139af991398646a16d0bc277bb7c05
AndrewBoldrin <andrewboldrin@hotmail.com>
2023-11-01 15:54:23 -0300

    feat: add footer

---
+++ src/components/core/Footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <div>
+      <p className="font-inter font-thin text-xs pb-4 pt-8 w-full md:max-w-[82.5rem] h-full m-auto md:max-xl:px-12 px-4 sm:max-md:px-0 border-t-2 border-dark-secondary">
+        Todas as informações dos filmes, séries e imagens são do{' '}
+        <a href="https://www.themoviedb.org/" className="text-blue-400">
+          TMDB
+        </a>
+      </p>
+    </div>
+  )
+}
--- src/components/core/Layout.tsx
+++ src/components/core/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { TopBar } from '../TopBar'
+import { Footer } from './Footer'
 
 export function Layout() {
   return (
     <>
       <TopBar />
       <Outlet />
+      <Footer />
     </>
   )
 }
