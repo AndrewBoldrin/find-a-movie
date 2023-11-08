7db41d1663081cece3f849c4b59222939fa5587f
AndrewBoldrin <andrewboldrin@hotmail.com>
2023-11-08 17:20:49 -0300

    fix: search bar size

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -11,8 +11,8 @@ export function SearchBar({ searchInput, setSearchInput }: Props) {
     <div className="max-w-[30rem] flex gap-4 mx-4 md:mx-auto mt-16 px-10 py-5 bg-dark-contrast-dark rounded-md shadow-lg hover:shadow-gray-700/50 focus-within:shadow-gray-700/50 transition ease-in-out delay-150">
       <input
         type="text"
-        className="w-full h-8 bg-dark-contrast-dark focus:outline-none"
-        placeholder="Pesquise filmes e séries por genero, nome..."
+        className="w-full h-8 bg-dark-contrast-dark focus:outline-none truncate "
+        placeholder="Pesquise filmes e séries"
         value={searchInput || ''}
         onChange={(e: React.FormEvent<HTMLInputElement>) =>
           setSearchInput(e.currentTarget.value)
