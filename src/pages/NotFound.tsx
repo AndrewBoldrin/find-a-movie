4530c74b42e282149e4e495d466aa8a773e71008
AndrewBoldrin <andrewboldrin@hotmail.com>
2023-11-08 14:22:59 -0300

    fix: page not found message position

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 export function NotFound() {
   return (
-    <div className="w-full h-full flex justify-center mt-12">
+    <div className="flex flex-1 justify-center items-center mt-12">
       <h1>404 Page not found</h1>
     </div>
   )
